1d72a4d8a3c5e6c8be3810612f6353b0afe0184f
zhzkal2 <reliya1541@gmail.com>
2024-11-15 17:19:31 +0900

    18807 새 방식의 재채점을 테스트하기 위한 문제

---
+++ 18807.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val (a, b) = readln().split(" ").map { it.toInt() }
+
+    val result = a+b
+
+    val n = readln().toInt()
+
+    val result2 = readln().split(" ").take(n).map { it.toInt() }.sorted()
+
+
+
+    println(result)
+    println(result2.joinToString(" "))
+
+}
