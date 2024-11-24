e74fb0c4d5bff77402b600c27deef74fa21471e7
zhzkal2 <reliya1541@gmail.com>
2024-11-24 22:07:41 +0900

    feat : 1920 수 찾기
    이분 탐색으로 구현

---
+++ kotlin_algo/1829.kt
@@ -0,0 +1,36 @@
+package kotlin_algo
+
+fun main(){
+
+    val n = readLine()!!.toInt()
+    val arrayA = readLine()!!.split(" ").map { it.toInt() }.sorted()
+    val m = readLine()!!.toInt()
+    val queries = readLine()!!.split(" ").map { it.toInt() }
+
+    fun binarySearch(taget:Int):Int{
+        var low =0
+        var high = arrayA.size - 1
+
+        while (low<=high){
+            val mid = low + (high - low) / 2
+            when{
+                arrayA[mid] ==taget ->{
+                    return 1}
+                arrayA[mid] <taget ->{
+                    low = mid+1
+                }
+                else->{
+                    high = mid - 1
+                }
+            }
+        }
+
+        return 0
+    }
+
+    val result = queries.map { binarySearch(it) }
+
+    println(result.joinToString("\n"))
+
+
+}
